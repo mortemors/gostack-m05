d3fdb8abd378522826adf23bdc60492790e3e257
Jeferson GB <jefersongb@outlook.com.br>
2020-01-28 23:17:17 -0300

    Exibição de informações obtidas pelo API do github e estilização para exibição dentro do próprio SPA.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { FaGithubAlt,FaPlus,FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import {Container, Form, SubmitButton} from './styles';
+import Container from '../../components/Container'
+
+import {Form, SubmitButton, List} from './styles';
 
 export default class Main extends Component {
   state = {
@@ -11,6 +14,20 @@ export default class Main extends Component {
     repositories: [],
     loading: false,
   };
+
+  componentDidMount(){
+    const repositories = localStorage.getItem('repositories');
+    if(repositories){
+      this.setState({repositories: JSON.parse(repositories)})
+    }
+  }
+
+  componentDidUpdate(_,prevState){
+    const{repositories} = this.state;
+    if(prevState.repositories !== repositories){
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInputChange = e => {
     this.setState({newRepo: e.target.value})
@@ -35,7 +52,7 @@ export default class Main extends Component {
     })
   }
   render(){
-    const {newRepo, loading} = this.state;
+    const {newRepo, loading, repositories} = this.state;
 
   return (
     <Container>
@@ -51,6 +68,15 @@ export default class Main extends Component {
 
         </SubmitButton>
       </Form>
+      <List>
+        {repositories.map(repository => (
+          <li key={repository.name}>
+            <span>{repository.name}</span>
+            <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
+          </li>
+        ))}
+
+      </List>
     </Container>
   );
   }
